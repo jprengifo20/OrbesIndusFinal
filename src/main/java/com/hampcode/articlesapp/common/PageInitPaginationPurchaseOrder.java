6333c57413532914337ab42a1ac1bf3370f9630f
iriarte20 <gonzalo.1256@hotmail.com>
2019-11-13 06:03:38 -0500

    VISTAS Y OTROS PURCHASE ORDERS

--- src/main/java/com/hampcode/articlesapp/common/PageInitPaginationPurchaseOrder.java
+++ src/main/java/com/hampcode/articlesapp/common/PageInitPaginationPurchaseOrder.java
@@ -8,21 +8,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import com.hampcode.articlesapp.model.PurchaseOrder;
 import com.hampcode.articlesapp.service.PurchaseOrderService;
 
 @Component
-public class PageInitPaginationPurcharseOrder {
+public class PageInitPaginationPurchaseOrder {
 
-
-	
 	@Autowired
 	private PurchaseOrderService purchaseorderService;
-	
-	/*@Autowired
-	private BookService bookService;*/
-	
 
 	// pagination
 	private static final int BUTTONS_TO_SHOW = 3;
@@ -39,37 +32,45 @@ public class PageInitPaginationPurcharseOrder {
 		 * return value of param. decreased by 1
 		 */
 		int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
-
+		
 		Page<PurchaseOrder> purchaseordersList = purchaseorderService.findAll(PageRequest.of(evalPage, evalPageSize));
+		PagerModel pager = new PagerModel(purchaseordersList.getTotalPages(), purchaseordersList.getNumber(), BUTTONS_TO_SHOW);
+		
+		initModelView.addObject("purchaseordersList", purchaseordersList);
+		initModelView.addObject("selectedPageSize", evalPageSize);
+		initModelView.addObject("pageSizes", PAGE_SIZES);
+		initModelView.addObject("pager", pager);
+		return initModelView;
+	}
+
+	public  ModelAndView initPaginationSearch(Optional<Integer> pageSize, Optional<Integer> page, 
+			String url, String product)  {
+		ModelAndView initModelView = new ModelAndView(url);
+		// If pageSize == null, return initial page size
+		int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+		/*
+		 * If page == null || page < 0 (to prevent exception), return initial size Else,
+		 * return value of param. decreased by 1
+		 */
+		int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+		
+		Page<PurchaseOrder> purchaseordersList = purchaseorderService.findByProduct(product, PageRequest.of(evalPage,evalPageSize));
+				/*
+				 
+				  */
+				 
 		PagerModel pager = new PagerModel(purchaseordersList.getTotalPages(), purchaseordersList.getNumber(), BUTTONS_TO_SHOW);
 		initModelView.addObject("purchaseordersList", purchaseordersList);
 		initModelView.addObject("selectedPageSize", evalPageSize);
 		initModelView.addObject("pageSizes", PAGE_SIZES);
 		initModelView.addObject("pager", pager);
-	
-		/*
 		
-		Page<Request> requestsList = requestService.findAll(PageRequest.of(evalPage, evalPageSize));
-		PagerModel pager2 = new PagerModel(requestsList.getTotalPages(), requestsList.getNumber(), BUTTONS_TO_SHOW);
-		initModelView.addObject("requestsList", requestsList);
-		initModelView.addObject("selectedPageSize", evalPageSize);
-		initModelView.addObject("pageSizes", PAGE_SIZES);
-		initModelView.addObject("pager", pager2);
-		
-		Page<Supplier> suppliersList = supplierService.findAll(PageRequest.of(evalPage, evalPageSize));
-		PagerModel pager3 = new PagerModel(suppliersList.getTotalPages(), suppliersList.getNumber(), BUTTONS_TO_SHOW);
-
-		initModelView.addObject("suppliersList", suppliersList);
-		initModelView.addObject("selectedPageSize", evalPageSize);
-		initModelView.addObject("pageSizes", PAGE_SIZES);
-		initModelView.addObject("pager", pager3);
-		
-		
-		*/
-	
 		return initModelView;
 	}
+	
+	
+	
 
 	
-	
 }
+
