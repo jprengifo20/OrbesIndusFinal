2a31d84a3eda8edea6206519e8a997959368df24
jprengifo <jprengifo@10.11.128.113>
2019-11-16 19:26:53 -0500

    Se agrego la datatos a data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,7 +21,7 @@ On April 19, 2017, React 360 V1.0.0 was released to the public.[13] This allowed
 
 
 INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$sr2ZuTYlZ.LsliI6sv.HqeH39WsA.gz9Fdr.S0CyCBByyPQ8TObvS','admin');
-INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$ZoqMrCUHNN8lE2g1zssyuucNhExlYwN06kM/Jo6vEBhyTQduQ7iCy','usercode');
+INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$ZoqMrCUHNN8lE2g1zssyuucNhExlYwN06kM/Jo6vEBhyTQduQ7iCy','jefe');
 
 
 
